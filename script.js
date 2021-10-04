968cdc4ab682556aa07da5bd430d60927f5be525
Somdotta07 <jhilick.s@gmail.com>
2021-10-04 15:46:31 +0200

    Added features for mobile-menu
    --Add link to the sections
    --Edit js file
    -- Add css properties

--- script.js
+++ script.js
@@ -7,34 +7,43 @@ toolbar.addEventListener('click', mobileMenu);
 
 function mobileMenu () {
     if (toolbar.classList.toggle('active')) {
-       let layout = document.getElementById("hamburger");
-        let toolbarButton = document.getElementById("toolbar-button");
+       let layout = document.getElementById("navbar-mobile");
+        let toolbarButton = document.getElementById("hamburger");
         let mobileLogo = document.getElementById("mobile-logo");
 
         mobileLogo.style.display = 'none';
-        toolbarButton.innerHTML =
-        `<a class="toolbar-button" href=# id="hamburger">
-        <img src="images/mobile-menu/exit.png" alt="exit">
-      </a>`;
-
-
+       toolbarButton.style.display ='none';
 
         layout.innerHTML += 
-        `<div class="background">
-        <a class="exit-link">
-            <img class="exit-img" src="images/mobile-menu/exit.png" alt="exit">
+        `<div class="menu">
+        <a class="exit-link" href="#" id="exit-link">
+        <img src="images/mobile-menu/exit-icon.png" alt="exit" id="exit-menu">
         </a>
 
         <ul class="menu-items">
-           <li class="menu-option"><a href="#">Portfolio</a></li>
-           <li class="menu-option"><a href="#">About</a></li> 
-           <li class="menu-option"><a href="#">Contact</a></li> 
+           <li class="menu-option"><a href="#portfolio-header">Portfolio</a></li>
+           <li class="menu-option"><a href="#about">About</a></li> 
+           <li class="menu-option"><a href="#contact-container">Contact</a></li> 
         </ul>
         </div>`
 
-
- 
-
+}else {
+  console.log('click here');
+}
 
 }
+
+let mobileNav = document.querySelectorAll('.menu-items');
+mobileNav.forEach(n => n.addEventListener("click", mobilecloseMenu));
+let menu = document.querySelector('.menu');
+
+
+function mobilecloseMenu() {
+  
+  if(x.classList.toggle('active')){
+    let x= document.getElementById('exit-link');
+   
+   
+    
+  }
 }
