6b1a2a7be0c11fe9cfa8d39ce2838051675d91ad
karelvanoordt <vanoordtkarel@gmail.com>
2021-10-05 02:24:18 -0500

    Add new html elements for mobile navigation menu

--- script.js
+++ script.js
@@ -1,51 +1,76 @@
 
 
+const navbarMenu = document.querySelector('.navbar-menu');
+const openMenu = document.querySelector('.toolbar-button');
+const closeMenu = document.querySelector('.exit-link');
 
-let toolbar = document.querySelector('.toolbar-button');
+openMenu.addEventListener('click', open);
+closeMenu.addEventListener('click',close);
 
-toolbar.addEventListener('click', mobileMenu);
-
-function mobileMenu () {
-    if (toolbar.classList.toggle('active')) {
-       let layout = document.getElementById("navbar-mobile");
-        let toolbarButton = document.getElementById("hamburger");
-        let mobileLogo = document.getElementById("mobile-logo");
-
-        mobileLogo.style.display = 'none';
-       toolbarButton.style.display ='none';
-
-        layout.innerHTML += 
-        `<nav class="menu">
-        <div class="nav-container">
-          <a class="exit-link" href="#" id="exit-link">
-            <img class="exit-img" src="images/mobile-menu/exit-icon.png" alt="exit" id="exit-menu">
-          </a>
-
-          <ul class="menu-items">
-            <li ><a class="menu-option" href="#portfolio-header">Portfolio</a></li>
-            <li ><a class="menu-option" href="#about">About</a></li> 
-            <li ><a class="menu-option" href="#contact-container">Contact</a></li> 
-          </ul>
-        </div>
-        </nav>`
-
-}else {
-  console.log('click here');
-}
+function open() {
+  navbarMenu.style.display = 'flex';
+  navbarMenu.style.top = '0';
 
 }
 
-let mobileNav = document.querySelectorAll('.menu-items');
-mobileNav.forEach(n => n.addEventListener("click", mobilecloseMenu));
-let menu = document.querySelector('.menu');
+function close() {
+  navbarMenu.style.top = '-100%';
+
+}
 
 
-function mobilecloseMenu() {
+
+
+
+
+
+
+
+
+// let toolbar = document.querySelector('.toolbar-button');
+
+// toolbar.addEventListener('click', mobileMenu);
+
+// function mobileMenu () {
+//     if (toolbar.classList.toggle('active')) {
+//        let layout = document.getElementById("navbar-mobile");
+//         let toolbarButton = document.getElementById("hamburger");
+//         let mobileLogo = document.getElementById("mobile-logo");
+
+//         mobileLogo.style.display = 'none';
+//        toolbarButton.style.display ='none';
+
+//         layout.innerHTML += 
+//         `<nav class="menu">
+//         <div class="nav-container">
+//           <a class="exit-link" href="#" id="exit-link">
+//             <img class="exit-img" src="images/mobile-menu/exit-icon.png" alt="exit" id="exit-menu">
+//           </a>
+
+//           <ul class="menu-items">
+//             <li ><a class="menu-option" href="#about">About</a></li> 
+//             <li ><a class="menu-option" href="#contact-container">Contact</a></li> 
+//           </ul>
+//         </div>
+//         </nav>`
+
+// }else {
+//   console.log('click here');
+// }
+
+// }
+
+// let mobileNav = document.querySelectorAll('.menu-items');
+// mobileNav.forEach(n => n.addEventListener("click", mobilecloseMenu));
+// let menu = document.querySelector('.menu');
+
+
+// function mobilecloseMenu() {
   
-  if(x.classList.toggle('active')){
-    let x= document.getElementById('exit-link');
+//   if(x.classList.toggle('active')){
+//     let x= document.getElementById('exit-link');
    
    
     
-  }
-}
+//   }
+// }
