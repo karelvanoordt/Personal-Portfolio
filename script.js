3753874126faac3338617bcc541d3974474403be
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2021-10-05 03:38:32 -0500

    Updatee Javascript file to fix linter issues

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-
-
 const navbarMenu = document.querySelector('.navbar-menu');
-
-
-
 const openMenu = document.querySelector('.toolbar-button');
-openMenu.addEventListener('click', open);
-
-const closeMenu = document.querySelector('.exit-link');
-closeMenu.addEventListener('click',close);
 
 function open() {
   navbarMenu.classList.remove('display-none');
 }
 
+openMenu.addEventListener('click', open);
+
+const closeMenu = document.querySelector('.exit-link');
+
 function close() {
   navbarMenu.classList.add('display-none');
 }
+
+closeMenu.addEventListener('click', close);
 
 const links = document.querySelectorAll('.menu-option');
 
 for (let i = 0; i < links.length; i += 1) {
   links[i].addEventListener('click', () => {
     navbarMenu.classList.add('display-none');
-  })
+  });
 }
